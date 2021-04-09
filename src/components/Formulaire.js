19ba1d0e9b99e07214e9e85d5366f3ab351b8c7e
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-09 16:59:23 +0200

    Chap 47 - Gestion de Nouveaux Events - Fini

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -7,7 +7,7 @@ class Formulaire extends Component {
   };
 
   createMessage = () => {
-      const { addMessage, pseudo } = this.props
+      const { addMessage, pseudo, length } = this.props
 
       const message = {
           pseudo,
@@ -17,7 +17,7 @@ class Formulaire extends Component {
       addMessage(message)
 
       //Reset
-      this.setState({ message: ''})
+      this.setState({ message: '', length})
   }
 
   handleSubmit = (event) => {
@@ -30,17 +30,27 @@ class Formulaire extends Component {
     const length = this.props.length - message.length
     this.setState({ message, length });
   }
+  handleKeyUp = (event) => {
+    if (event.key === 'Enter') {
+        this.createMessage()
+    }
+  }
 
   render() {
     return (
       <form className="form" onSubmit={this.handleSubmit}>
         <textarea 
         value={this.state.message} 
-        onChange={this.handleChange} 
+        onChange={this.handleChange}
+        onKeyUp={this.handleKeyUp} 
         required 
         maxLength={this.props.length} />
-        <div className='info'>{ this.state.length }</div>
-        <button type="submit">Envoyer!</button>
+        <div className='info'>
+        { this.state.length }
+        </div>
+        <button type="submit">
+        Envoyer!
+        </button>
       </form>
     );
   }
