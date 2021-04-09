d0f7ae47f71389c89d5b84740804fcb5f85aa12e
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-09 14:51:03 +0200

    Chap 43 - Decouverte de React Router V4

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import App from './App'
 import Connexion from './components/Connexion'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Connexion />, document.getElementById('root'))
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import NotFound from './components/NotFound'
+
+const Root = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Connexion}></Route>
+      <Route path='/pseudo/:pseudo' component={App}></Route>
+      <Route component={NotFound}></Route>
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
