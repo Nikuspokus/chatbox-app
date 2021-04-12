e04315294eb4209e2dfe1a1815107791677a78d9
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-12 11:17:06 +0200

    Chap 50 - Connecter firebase et react

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import Rebase from 're-base'
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyB3MnqTk2CWlfXwPZWjV9uK-tgFLYM46oI",
+    authDomain: "chatbox-app-a9f4c.firebaseapp.com",
+    databaseURL: "https://chatbox-app-a9f4c-default-rtdb.europe-west1.firebasedatabase.app",
+})
+
+// constante permettant de linker rebase avec fire base
+const base = Rebase.createClass(firebase.database())
