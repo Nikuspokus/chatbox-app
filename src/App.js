c9164d9114270b1af377bfa6615a38605b5d9547
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-12 15:32:29 +0200

    Chap 51 - les ref dans react: createRef() - FINI

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
-import React, { Component } from "react";
+import React, { Component, createRef} from "react";
 import "./App.css";
 import Formulaire from "./components/Formulaire";
 import Message from "./components/Message";
 
+// Firebase
+import base from "./base";
+
 class App extends Component {
   state = {
-    messages : {},
-    pseudo: this.props.match.params.pseudo
+    messages: {},
+    pseudo: this.props.match.params.pseudo,
   }
 
-  addMessage = message => {
-    const messages = {...this.state.messages}
-    messages[`message-${Date.now()}`] = message
-    this.setState({ messages })
+  messagesRef = createRef()
+
+  componentDidMount() {
+    // Permet de synchroniser un state avec la base de donnée
+    base.syncState('/', {
+      context: this,
+      state: "messages",
+    })
   }
+
+  componentDidUpdate() {
+    // la ref correspond au dernier message envoyé grace à .current
+    const ref = this.messagesRef.current
+    // defini que le haut du scroll = taille du scroll antier
+    ref.scrollTop = ref.scrollHeight
+  }
+
+  addMessage = (message) => {
+    const messages = { ...this.state.messages };
+    messages[`message-${Date.now()}`] = message;
+    this.setState({ messages });
+  };
 
   render() {
-    
-    const messages = Object
-      .keys(this.state.messages)
-      .map(key => (
+    const messages = Object.keys(this.state.messages).map((key) => (
       <Message
-      key={key}
-      message={this.state.messages[key].message}
-      pseudo={this.state.messages[key].pseudo}/>
-      ))
+        key={key}
+        message={this.state.messages[key].message}
+        pseudo={this.state.messages[key].pseudo}
+      />
+    ));
 
-
-    console.log(messages);
     return (
       <div className="box">
         <div>
-          <div className="messages">
-            <div className="message">
-              { messages }
-            </div>
+          <div className="messages" ref={this.messagesRef}>
+            <div className="message">{messages}</div>
           </div>
         </div>
-        <Formulaire 
+        <Formulaire
           length={140}
-          pseudo = {this.state.pseudo}
+          pseudo={this.state.pseudo}
           addMessage={this.addMessage}
         />
       </div>
--- src/base.js
+++ src/base.js
@@ -2,10 +2,15 @@ import Rebase from 're-base'
 import firebase from 'firebase/app'
 import 'firebase/database'
 
+// const firebaseApp = firebase.initializeApp({
+//     apiKey: "AIzaSyB3MnqTk2CWlfXwPZWjV9uK-tgFLYM46oI",
+//     authDomain: "chatbox-app-a9f4c.firebaseapp.com",
+//     databaseURL: "https://chatbox-app-a9f4c-default-rtdb.europe-west1.firebasedatabase.app",
+// })
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB3MnqTk2CWlfXwPZWjV9uK-tgFLYM46oI",
-    authDomain: "chatbox-app-a9f4c.firebaseapp.com",
-    databaseURL: "https://chatbox-app-a9f4c-default-rtdb.europe-west1.firebasedatabase.app",
+    apiKey: "AIzaSyDUDOKnsyn14F8wGxHWIJRHXD7AQM7cqyk",
+    authDomain: "chatbox-app-672ee.firebaseapp.com",
+    databaseURL: "https://chatbox-app-672ee-default-rtdb.firebaseio.com",
 })
 
 // constante permettant de linker rebase avec fire base
